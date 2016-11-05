60797db6821dd87e53540520b3e213fdaa0fc518
remotejob <support@mazurov.eu>
2016-11-05 05:26:07 +0200

    dev

---
+++ check_logfile_existense/check_logfile_existense.go
@@ -0,0 +1,12 @@
+package check_logfile_existense
+
+//Check simple checl if logfile exist
+func Check(logfile string) bool {
+
+	var exist bool
+
+	exist = false
+
+	return exist
+
+}
---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func init() {
+
+	// exist := check_logfile_existense("log.csv")
+
+}
+
+func middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+
+		h.ServeHTTP(rw, req)
+	})
+}
+
+func loghandler(rw http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(rw, "Hello request")
+}
+
+func main() {
+
+	h := middleware(http.HandlerFunc(loghandler))
+	log.Fatal(http.ListenAndServe(":8080", h))
+
+}
